87ced2d1ca2e79fe60d12cb43855870d5da42e7b
Sopheak Saing <sopheak.saing@khalibre.com>
2024-05-07 14:10:45 +0700

    simple testing accordian works correctly

--- cypress/e2e/foundamentals.cy.js
+++ cypress/e2e/foundamentals.cy.js
@@ -3,4 +3,10 @@ describe('template spec', () => {
     cy.visit('/fundamentals')
     cy.get('[data-test="fundamentals-header"]').should('contain.text', 'Testing Fundamentals')
   })
+  it('Accordian works correctly', () => {
+    cy.visit('/fundamentals')
+    cy.contains('Your tests will exist in a describe block.').should('not.be.visible')
+    cy.get('[data-test="accordion-item-1"]').click()
+    cy.contains('Your tests will exist in a describe block.').should('be.visible')
+  })
 })
