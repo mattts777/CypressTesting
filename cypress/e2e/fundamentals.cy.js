5b3d9a499cd451883870111c59ab5dd538814d11
Maryam Alimohammadi <maryam.alimohammadi70@gmail.com>
2024-06-30 16:12:28 +0330

    before Each function

--- cypress/e2e/fundamentals.cy.js
+++ cypress/e2e/fundamentals.cy.js
@@ -1,5 +1,18 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('fundamentals test', () => {
+  beforeEach( () => {
+    cy.visit('/fundamentals')
+  }
+  )
+  
+  it('contains correct header', () => {
+    cy.get('[data-test="fundamentals-header"]').contains(/testing fundamentals/i)
+  })
+  it('Accordian works correctly', () => {
+    cy.contains(/Your tests will exist in a describe block/i).should('not.be.visible')
+    cy.get('[data-test="accordian-item-1"] div[role="button"]').click()
+    cy.contains(/Your tests will exist in a describe block/i).should('be.visible')
+    cy.get('[data-test="accordian-item-1"] div[role="button"]').click()
+    cy.contains(/Your tests will exist in a describe block/i).should('not.be.visible')
+
   })
 })
