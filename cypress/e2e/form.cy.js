e7cee57987ae3db73909755154b065ac6940ce7f
isaclaleo <isac.renovato@librerialeo.com.mx>
2024-07-26 17:02:32 -0600

    step 4

--- cypress/e2e/form.cy.js
+++ cypress/e2e/form.cy.js
@@ -4,9 +4,21 @@ describe('Cypress With Forms', () => {
   })
   it('Step 1: Get Form', () => {
     cy.contains(/Testing Forms/i)
+    cy.getDataTest('subscribe-form').find('input').as('subscribe-input')
     // Step 2 Type to Form
-    cy.getDataTest('subscribe-form').find('input').type('isacte@gmail.com')
-    // Step 3) Subscribe Min.1:15:14
+    cy.get('@subscribe-input').type('isacte@gmail.com')
+    cy.contains(/Successfully subbed: isacte@gmail.com!/i).should('not.exist')
+    // Step 3) Subscribe Min.1:25
     cy.getDataTest('susbcribe-button').click()
+    // Step 4) Test success/fail
+    cy.contains(/Successfully subbed: isacte@gmail.com!/i).should('exist')
+    cy.wait(3000)
+    cy.contains(/Successfully subbed: isacte@gmail.com!/i).should('not.exist')
+    
+    // Invalid email:
+    cy.get('@subscribe-input').type('isacte@gmail.mx')
+    cy.getDataTest('susbcribe-button').click()
+    cy.contains(/Invalid email: isacte@gmail.mx/i).should('exist')
+
   });
 })
