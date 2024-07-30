c2537caca698ffad526b12842a6abbb680054913
isaclaleo <isac.renovato@librerialeo.com.mx>
2024-07-30 14:14:13 -0600

    testing cypress Forms done at  5 of 5 steps

--- cypress/e2e/form.cy.js
+++ cypress/e2e/form.cy.js
@@ -2,30 +2,41 @@ describe('Cypress With Forms', () => {
   beforeEach(() => {
     cy.visit('/forms')
   })
-  it('Step 1: Get Form', () => {
-    cy.contains(/Testing Forms/i)
-    cy.getDataTest('subscribe-form').find('input').as('subscribe-input')
-    // Step 2 Type to Form
-    cy.get('@subscribe-input').type('isacte@gmail.com')
-    cy.contains(/Successfully subbed: isacte@gmail.com!/i).should('not.exist')
-    // Step 3) Subscribe Min.1:25
-    cy.getDataTest('susbcribe-button').click()
-    // Step 4) Test success/fail
-    cy.contains(/Successfully subbed: isacte@gmail.com!/i).should('exist')
-    cy.wait(3000)
-    cy.contains(/Successfully subbed: isacte@gmail.com!/i).should('not.exist')
-    
-    // Invalid email:
-    cy.get('@subscribe-input').type('isacte@gmail.mx')
-    cy.getDataTest('susbcribe-button').click()
-    cy.contains(/Invalid email: isacte@gmail.mx/i).should('exist')
-  });
-  it('should  Step 1: Get form', () => {
+  it('Step 1) should Get form', () => {
     cy.contains(/Testing Form/i)
     cy.getDataTest('subscribe-form').find('input').as('subscribe-input')
   });
-
-  it('should Step 2: Type Form', () => {
-    
+  it('Step 2) should type form', () => {
+    cy.getDataTest('subscribe-form').find('input').as('subscribe-input')
+    cy.get('@subscribe-input').type('isacte@gmail.com')
+  });
+  it('Step 3)should Susbcribe', () => {
+    cy.getDataTest('subscribe-form').find('input').as('subscribe-input')
+    cy.get('@subscribe-input').type('isacte@gmail.com')
+    cy.getDataTest('susbcribe-button').click()
+  });
+  it('Step 4) Test should success email', () => {
+    cy.getDataTest('subscribe-form').find('input').as('subscribe-input')
+    cy.get('@subscribe-input').type('isacte@gmail.com')
+    cy.contains(/Successfully subbed: isacte@gmail.com!/i).should('not.exist')
+    cy.getDataTest('susbcribe-button').click()
+    cy.contains(/Successfully subbed: isacte@gmail.com!/i).should('exist')
+    cy.wait(3000)
+    cy.contains(/Successfully subbed: isacte@gmail.com!/i).should('not.exist')
+  });
+  it('Step 4) Test should fail email', () => {
+    // Invalid email:
+    cy.getDataTest('subscribe-form').find('input').as('subscribe-input')
+    cy.get('@subscribe-input').type('isacte@gmail.mx')
+    cy.contains(/Invalid email: isacte@gmail.mx/i).should('not.exist')
+    cy.getDataTest('susbcribe-button').click()
+    cy.contains(/Invalid email: isacte@gmail.mx/i).should('exist')
+    cy.wait(3000)
+    cy.contains(/Invalid email: isacte@gmail.mx/i).should('not.exist')
+  });
+  it('Step 5) should fail', () => {
+    cy.contains(/fail!/i).should('not.exist')
+    cy.getDataTest('susbcribe-button').click()
+    cy.contains(/fail!/i).should('exist')
   });
 })
