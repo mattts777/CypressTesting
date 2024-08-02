e46360458263ea121649b93de4de0b14cf818f9c
isactes <fidel.renovato@gmail.com>
2024-08-01 19:30:34 -0600

    min 1:40 examples

--- cypress/e2e/example.cy.js
+++ cypress/e2e/example.cy.js
@@ -5,4 +5,33 @@ describe('Cypress mor examples', () => {
   it('should conatins', () => {
     cy.contains(/Examples/i)
   });
+  it('multi-page testing Pathname', () => {
+    cy.getDataTest('nav-why-cypress').click()
+    cy.location('pathname').should('equal', '/')
+
+    cy.getDataTest('nav-overview').click()
+    cy.location('pathname').should('equal', '/overview')
+
+    cy.getDataTest('nav-fundamentals').click()
+    cy.location('pathname').should('equal', '/fundamentals')
+
+    cy.getDataTest('nav-forms').click()
+    cy.location('pathname').should('equal', '/forms')
+    
+    // cy.getDataTest('nav-examples').click()
+    // cy.location('pathname').should('equal', '/examples')
+
+    cy.getDataTest('nav-componet').click()
+    cy.location('pathname').should('equal', '/component')
+
+    cy.getDataTest('nav-best-practices').click()
+    cy.location('pathname').should('equal', '/best-practices')
+  });
+  it('should intercepts', () => {
+    cy.intercept('POST', 'http://localhost:3000/examples', {
+      body:{
+        message: '✨ successflly intercept'
+      }
+    })
+  });
 })
